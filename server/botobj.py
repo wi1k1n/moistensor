d81b70e3d6ad7886255b92c9d734a2df00334e9f
Ilya Mazlov <mazlov.i.a@gmail.com>
2022-01-10 00:35:55 +0100

    bot admin/user authentication

--- server/botobj.py
+++ server/botobj.py
@@ -21,16 +21,18 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 class TGUser:
-    def __init__(self, uid):
-        self.uid = uid
+    def __init__(self, uid: int, pmChatId: int, admin: bool = False):
+        self.uid: int = uid
+        self.admin: bool = admin
+        self.pmChatId: int = pmChatId
 
 class SubscribedChat:
-    def __init__(self, chat_id):
-        self.chat_id = chat_id
+    def __init__(self, chat_id: int):
+        self.chat_id: int = chat_id
 
 class TelegramBot:
     def __init__(self, _token: str):
-        self.TOKEN = _token
+        self.TOKEN: str = _token
         self.WAITING_FOR_PASSWORD,\
             self.STREAMEVENTS,\
             self.IGNORE = range(3)
@@ -55,6 +57,7 @@ class TelegramBot:
             entry_points=[CommandHandler('start', self.start)],
             states={
                 self.WAITING_FOR_PASSWORD: [
+                    CommandHandler('start', self.start),
                     MessageHandler(Filters.text, self.checkPassCode),
                 ],
                 self.STREAMEVENTS: [
@@ -67,10 +70,8 @@ class TelegramBot:
         self.updater.start_polling()
         if blockThread:
             self.updater.idle()
-
     def foreground(self):
         self.updater.idle()
-
     def stopBot(self):
         self.updater.stop()
 
@@ -78,25 +79,45 @@ class TelegramBot:
         dg = hashlib.sha224(secrets.token_bytes(8) + str(usr.id).encode('utf-8')).digest()
         return str(int.from_bytes(random.sample(dg, 4), 'big'))
 
-    def start(self, upd: Update, ctx: CallbackContext) -> int:
+    def start(self, upd: Update, ctx: CallbackContext) -> int | None:
         if upd.effective_user.is_bot:
             return self.IGNORE
+        if upd.effective_user.id in self.authorizedUsers:
+            upd.message.reply_text('You are already authorized. Try /help command.')
+            return
+        if upd.effective_chat.type != 'private':
+            return
+
         upd.message.reply_text(
-            "Welcome to Moistensor Bot. Please authorize yourself with a code from the console."
+            "Welcome to Moistensor Bot. Please authorize yourself as administrator with a code from the console. "
+            "Or ask administrator for the authorization code."
         )
-        passCode = self.generatePassCode(upd.effective_user)
-        ctx.user_data['passCode'] = passCode
+        adminPassCode = self.generatePassCode(upd.effective_user)
+        ctx.user_data['adminPassCode'] = adminPassCode
         ctx.user_data['authAttempts'] = self.AUTH_INITIAL_ATTEMPTS
         ctx.user_data['banned'] = False
-        print('New user: [' + str(upd.effective_user.id) + '] ' + upd.effective_user.name + ' | Passcode: ' + passCode)
+        print('New user: [{0}] {1} | Passcode: {2}'.format(upd.effective_user.id, upd.effective_user.name, adminPassCode))
+
+        # Check if there's already administrators
+        adminIds = [tguser.pmChatId for (_, tguser) in self.authorizedUsers.items() if tguser.admin]
+        if len(adminIds):
+            userPassCode = self.generatePassCode(upd.effective_user)
+            while userPassCode == adminPassCode:
+                userPassCode = self.generatePassCode(upd.effective_user)
+            ctx.user_data['userPassCode'] = userPassCode
+            self.broadcastMessageToChats(adminIds, 'New user: [{0}] {1} | Passcode: {2}'.format(upd.effective_user.id, upd.effective_user.name, userPassCode))
+
         return self.WAITING_FOR_PASSWORD
 
-    def alarm(ctx: CallbackContext) -> None:
-        """Send the alarm message."""
-        ctx.bot.send_message(ctx.job.context, text='Beep!')
-
     def checkPassCode(self, upd: Update, ctx: CallbackContext) -> int:
-        if upd.message.text != ctx.user_data['passCode']:
+        # Assumed to not being accessible to bots and not in private chats (as handled earlier)
+        admin = False
+        if 'adminPassCode' in ctx.user_data and upd.message.text == ctx.user_data['adminPassCode']:
+            msg = 'Successfully authorized as Admin!'
+            admin = True
+        elif 'userPassCode' in ctx.user_data and upd.message.text == ctx.user_data['userPassCode']:
+            msg = 'Successfully authorized!'
+        else:
             ctx.user_data['authAttempts'] -= 1
             if ctx.user_data['authAttempts'] <= 0:
                 ctx.user_data['banned'] = True
@@ -109,11 +130,10 @@ class TelegramBot:
                     'Wrong pass code. Try again (' + str(ctx.user_data['authAttempts']) + ' attempts left)'
                 )
             return self.WAITING_FOR_PASSWORD
-        upd.message.reply_text(
-            'Successfully authorized! Listening for the events.'
-        )
-        self.authorizedUsers[upd.effective_user.id] = TGUser(upd.effective_user.id)
-        self.subscribedChats[upd.message.chat_id] = SubscribedChat(upd.message.chat_id)
+
+        upd.message.reply_text(msg)
+        self.authorizedUsers[upd.effective_user.id] = TGUser(upd.effective_user.id, upd.effective_chat.id, admin)
+        self.subscribedChats[upd.message.chat_id] = SubscribedChat(upd.message.chat_id)  # TODO: remove this as it needs to be handled smarter through user interaction
 
         return self.STREAMEVENTS
 
@@ -123,26 +143,29 @@ class TelegramBot:
         )
         return self.STREAMEVENTS
 
-    def broadcastMessageToSubscribers(self, msg: str) -> None:
-        assert len(self.subscribedChats) < 1e3, 'Too many subscribers, cannot run multithreaded message sending. Contact developers!'
-        def sendMessage(chatId: int, msg: str) -> bool:
+    def broadcastMessageToChats(self, chatIds: List[int], msg: str) -> bool:
+        assert len(chatIds) < 1e3, 'Too large user list, cannot run multithreaded message sending. Contact developers!'
+        def sendMessage(chatId: int) -> bool:
             for i in range(self.SEND_MESSAGE_ATTEMPTS):
                 try:
                     m: Message = self.updater.bot.send_message(chat_id, msg)
                     return True if m else False
                 except Exception as e:
-                    print('Exception raised when sending to chat_id={0} attemp#{2}:\n\t>>{1}'.format(chatId, str(e), i))
+                    print('Exception raised when sending to chat_id={0} attempt#{2}:\n\t>>{1}'.format(chatId, str(e), i))
                     time.sleep(self.SEND_MESSAGE_REATTEMT_DELAY)
             return False
 
         threads = []
-        for chat_id in self.subscribedChats:
-            thr = threading.Thread(target=sendMessage, args=(chat_id, msg))
+        for chat_id in chatIds:
+            thr = threading.Thread(target=sendMessage, args=(chat_id,))
             threads.append(thr)
             thr.start()
 
         for thr in threads:
             thr.join()
+
+    def broadcastMessageToSubscribers(self, msg: str) -> None:
+        self.broadcastMessageToChats(list(self.subscribedChats.keys()), msg)
 
     def handleSerialUpdate(self, msg: dict) -> None:
         if not msg:
